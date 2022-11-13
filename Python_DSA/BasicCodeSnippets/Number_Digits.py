9326b22384474330591a9f60da326821346d228d
Angry Boy <suvxdas@gmail.com>
2022-11-13 17:53:20 +0530

    find the todatl number of digits in a number

---
+++ Python_DSA/BasicCodeSnippets/Number_Digits.py
@@ -0,0 +1,9 @@
+
+# find the todatl number of digits in a number
+
+n=int(input("Enter number:"))
+count=0
+while(n>0):
+    count=count+1
+    n=n//10
+print(f"The number of digits in the number are: {count}")
