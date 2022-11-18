dcec00be9e45b200c76d942ad6185d7a0cd12c08
Angry Boy <suvxdas@gmail.com>
2022-11-18 20:43:13 +0530

    program to add two numbers

---
+++ Python_DSA/BasicCodeSnippets/add_numbers.py
@@ -0,0 +1,37 @@
+# Python3 program to add two numbers
+ 
+num1 = 15
+num2 = 12
+ 
+# Adding two nos
+sum = num1 + num2
+ 
+# printing values
+print("Sum of {0} and {1} is {2}" .format(num1, num2, sum))
+
+# ======= METHOD 2 =============
+
+# number1 = input("First number: ")
+# number2 = input("\nSecond number: ")
+ 
+# # Adding two numbers
+# # User might also enter float numbers
+# sum = float(number1) + float(number2)
+ 
+# # Display the sum
+# # will print value in float
+# print("The sum of {0} and {1} is {2}" .format(number1, number2, sum))
+
+# ======== METHOD 3 =============
+
+
+# if __name__ == "__main__" :
+   
+#   num1 = 15
+#   num2 = 12
+   
+#   # Adding two numbers
+#   sum_twoNum = lambda num1, num2 : num1 + num2
+   
+#   # printing values
+#   print("Sum of {0} and {1} is {2};" .format(num1, num2, sum_twoNum(num1, num2)))
