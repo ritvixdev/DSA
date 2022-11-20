851c264a6a2fcbce640e1edd4f3ce1a6a6914f5c
Angry Boy <suvxdas@gmail.com>
2022-11-20 10:50:41 +0530

    one

---
+++ Python_DSA/BasicCodeSnippets/BasicPrograms/Number_Digits.py
@@ -0,0 +1,14 @@
+
+# find the todatl number of digits in a number
+
+# n=int(input("Enter number:"))
+# count=0
+# while(n>0):
+#     count=count+1
+#     n=n//10
+# print(f"The number of digits in the number are: {count}")
+
+lst = [2,4,8,5,9,35,3,6,76,45]
+
+lst.sort()
+print(lst[-2])
