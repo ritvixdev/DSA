0df99cacf4a8c2d50b45e2fc6437d2ed75008bbc
Angry Boy <suvxdas@gmail.com>
2022-11-21 11:30:48 +0530

    u

--- Python_DSA/BasicCodeSnippets/BasicPrograms/Number_Digits.py
+++ Python_DSA/BasicCodeSnippets/BasicPrograms/Number_Digits.py
@@ -16,13 +16,8 @@ y = [[9,8,7],
      [6,5,4],
      [3,2,1]]
 
-result = [[0,0,0],
-          [0,0,0],
-          [0,0,0]]
+# result = [[0,0,0],
+#           [0,0,0],
+#           [0,0,0]]
 
-for i in range (len(x)):
-    for j in range (len(y)):
-        result[i][j] = x[i][j] + y[i][j]
-        
-for r in result:
-    print(r)
+
