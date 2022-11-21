49e75e64a27d594cd6e1040b8d8333a266eac0f6
Angry Boy <suvxdas@gmail.com>
2022-11-21 11:15:13 +0530

    b

--- Python_DSA/BasicCodeSnippets/BasicPrograms/Number_Digits.py
+++ Python_DSA/BasicCodeSnippets/BasicPrograms/Number_Digits.py
@@ -10,11 +10,12 @@
 
 lst = [2,4,8,5,9,35,3,6,76,5,45,47,5,30]
 
-n = 2
-m = 7
+n = int(len(lst))
+sum1  = 0
+lst2 = []
 
-for i in range(n,m+1):
-    if i % 2 != 0:
-        print(i,end=", ")
+for i in range(n):
+    sum1 += lst[i]
+    lst2.append(sum1)
     
-    
+print(lst2)
