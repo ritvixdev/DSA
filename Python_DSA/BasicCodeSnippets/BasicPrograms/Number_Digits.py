29426d278145dbb3c0efd32f926d39e2d45aafaf
Angry Boy <suvxdas@gmail.com>
2023-01-07 07:50:33 +0530

    changed

--- Python_DSA/BasicCodeSnippets/BasicPrograms/Number_Digits.py
+++ Python_DSA/BasicCodeSnippets/BasicPrograms/Number_Digits.py
@@ -8,16 +8,21 @@
 #     n=n//10
 # print(f"The number of digits in the number are: {count}")
 
-x = [[1,2,3],
-     [4,5,6],
-     [7,8,9]]
+# x = [[1,2,3],
+#      [4,5,6],
+#      [7,8,9]]
 
-y = [[9,8,7],
-     [6,5,4],
-     [3,2,1]]
+# y = [[9,8,7],
+#      [6,5,4],
+#      [3,2,1]]
 
 # result = [[0,0,0],
 #           [0,0,0],
 #           [0,0,0]]
 
 
+str = "khokho"
+n = len(str)
+new_str  = str.replace('o','', 2)
+
+print(new_str)
