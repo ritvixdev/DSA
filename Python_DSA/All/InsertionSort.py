749074f0aed0d1fbabe995a03f4f8deed9e96037
Angry Boy <suvxdas@gmail.com>
2023-01-08 12:22:05 +0530

    program for implementation of Insertion Sort

--- Python_DSA/All/InsertionSort.py
+++ Python_DSA/All/InsertionSort.py
@@ -1,5 +1,31 @@
 # Python program for implementation of Insertion Sort
+
+# Time Complexity of InsertionSort:     |   Space Complexity   
+#       BEST        AVERAGE     WORST   |   WORST
+#       (n)         (n^2)       (n^2)   |   O(1)   
+
+
+def insertionSort(arr):
+     
+    if (n := len(arr)) <= 1:
+      return
+    for i in range(1, n):
+         
+        key = arr[i]
+      
+        j = i-1
+        while j >=0 and key < arr[j] :
+                arr[j+1] = arr[j]
+                j -= 1
+        arr[j+1] = key
  
+arr = [12, 11, 13, 5, 6]
+insertionSort(arr)
+print(arr)
+
+
+#Program explained:
+
 # Function to do insertion sort
 def insertionSort(arr):
      
