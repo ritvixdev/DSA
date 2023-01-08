8468926066c5fcea7fe5d5cff1899e89a53f3485
Angry Boy <suvxdas@gmail.com>
2023-01-08 14:21:24 +0530

    l

---
+++ Python_DSA/All/LinerSearch.py
@@ -0,0 +1,35 @@
+# Program to inpliment Linear Search:
+
+def search(arr, x):
+ 
+    for i in range(len(arr)):
+        if arr[i] == x:
+            return i
+ 
+    return -1
+
+arr = [1,4,7,8,9,3,4,5,6,7]
+target = 5
+print(search(arr,5))
+
+
+# Searching an element in a list/array in python
+# can be simply done using \'in\' operator
+# Example:
+# if x in arr:
+#   print arr.index(x)
+ 
+# If you want to implement Linear Search in python
+ 
+# Linearly search x in arr[]
+# If x is present then return its location
+# else return -1
+ 
+def search(arr, x):
+ 
+    for i in range(len(arr)):
+ 
+        if arr[i] == x:
+            return i
+ 
+    return -1
