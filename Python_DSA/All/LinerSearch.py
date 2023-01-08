143afe23a6804d4133417fb274a03b414d1b2ff7
Angry Boy <suvxdas@gmail.com>
2023-01-08 14:22:12 +0530

    y

--- Python_DSA/All/LinerSearch.py
+++ Python_DSA/All/LinerSearch.py
@@ -2,7 +2,7 @@
 
 # Time Complexity of QuickSort:         |   Space Complexity   
 #       BEST        AVERAGE     WORST   |   WORST
-#       0(1)     nlog(n)     (n^2)   |   log(n)    
+#       O(1)     nlog(n)     (n^2)   |   log(n)    
 
 
 def search(arr, x):
